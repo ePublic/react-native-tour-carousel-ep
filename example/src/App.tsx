6914200d44b564595ddd6894bf4af58916a5e098
NicolaLaptopEP <nicola.turco@epublic.it>
2025-07-30 15:24:34 +0200

    nome progetto modificato per consentire upload nel registro npm

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import TourCarousel from 'react-native-tour-carousel';
+import TourCarousel from 'react-native-tour-carousel-ep';
 
 export default function App() {
   const data = [
